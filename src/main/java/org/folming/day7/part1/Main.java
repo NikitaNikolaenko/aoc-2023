68a054be6f308b987dc1829bd7812aa2b450db1e
Nikita Nikolaenko <6870920@gmail.com>
2023-12-07 00:45:38 -0500

    Add Day 7 Part 1 solution

---
+++ src/main/java/org/folming/day7/part1/Main.java
@@ -0,0 +1,170 @@
+package org.folming.day7.part1;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+enum HandType {
+    HIGH_CARD,
+    ONE_PAIR,
+    TWO_PAIR,
+    THREE_OF_A_KIND,
+    FULL_HOUSE,
+    FOUR_OF_A_KIND,
+    FIVE_OF_A_KIND
+}
+
+enum Card {
+    TWO('2'),
+    THREE('3'),
+    FOUR('4'),
+    FIVE('5'),
+    SIX('6'),
+    SEVEN('7'),
+    EIGHT('8'),
+    NINE('9'),
+    TEN('T'),
+    JACK('J'),
+    QUEEN('Q'),
+    KING('K'),
+    ACE('A');
+
+    private final char c;
+
+    Card(char c) {
+        this.c = c;
+    }
+
+    public static Card ofChar(char c){
+        Card[] values = Card.values();
+        for (Card value : values) {
+            if(value.c == c){
+                return value;
+            }
+
+        }
+        throw new IllegalArgumentException("Card not found");
+    }
+}
+
+class Hand implements Comparable<Hand> {
+    public Card[] cards;
+    public long bid;
+    private HandType handType;
+
+    public Hand(final Card[] cards, final long bid) {
+        this.cards = cards;
+        this.bid = bid;
+        calculateHandType();
+    }
+
+    @Override
+    public int compareTo(Hand o) {
+        if (this.handType.ordinal() > o.handType.ordinal()) {
+            return 1;
+        } else if (this.handType.ordinal() < o.handType.ordinal()) {
+            return -1;
+        }
+
+        for (int i = 0; i < cards.length; i++) {
+            if (this.cards[i].ordinal() > o.cards[i].ordinal()) {
+                return 1;
+            } else if (this.cards[i].ordinal() < o.cards[i].ordinal()) {
+                return -1;
+            }
+        }
+
+        return 0;
+    }
+
+    private void calculateHandType() {
+        Map<Card, Integer> cardMap = new HashMap<>();
+        for (final Card card : cards) {
+            cardMap.merge(card, 1, Integer::sum);
+        }
+
+        if (cardMap.size() == 1) {
+            handType = HandType.FIVE_OF_A_KIND;
+            return;
+        }
+
+        if (cardMap.size() == 5) {
+            handType = HandType.HIGH_CARD;
+            return;
+        }
+
+        boolean possibleFullHouse = false;
+        boolean possibleThreeOfAKind = false;
+        boolean possibleTwoPair = false;
+
+        for (final int value : cardMap.values().stream().sorted(Comparator.reverseOrder()).toList()) {
+            if (value == 4) {
+                handType = HandType.FOUR_OF_A_KIND;
+                return;
+            }
+
+            if (value == 3) {
+                possibleFullHouse = true;
+                possibleThreeOfAKind = true;
+            }
+
+            if (value == 2) {
+                if (possibleFullHouse) {
+                    handType = HandType.FULL_HOUSE;
+                    return;
+                }
+
+                if (possibleTwoPair) {
+                    handType = HandType.TWO_PAIR;
+                    return;
+                }
+                possibleTwoPair = true;
+            }
+        }
+
+        if (possibleThreeOfAKind) {
+            handType = HandType.THREE_OF_A_KIND;
+            return;
+        }
+
+        if (possibleTwoPair) {
+            handType = HandType.ONE_PAIR;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        final List<String> lines = Files.readAllLines(Path.of("src/main/java/org/folming/day7/input"), StandardCharsets.UTF_8);
+
+        final List<Hand> hands = new ArrayList<>(lines.size());
+        for (final var line : lines) {
+            final String[] temp = line.split(" ");
+            final String cardsString = temp[0];
+            final long bid = Long.parseLong(temp[1]);
+
+            final Card[] cards = new Card[cardsString.length()];
+            for (int i = 0; i < cardsString.length(); i++) {
+                cards[i] = Card.ofChar(cardsString.charAt(i));
+            }
+
+            hands.add(new Hand(cards, bid));
+        }
+
+        Collections.sort(hands);
+
+        long result = 0;
+
+        for (int i = 0; i < hands.size(); i++) {
+            result += hands.get(i).bid * (i + 1);
+        }
+
+        System.out.println(result); // 248179786
+    }
+}
